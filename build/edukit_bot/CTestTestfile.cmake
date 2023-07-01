c0765e2e0c9ac4a9d5c7b9814f6b8610b912fe10
Lukewal99 <lukewal99@gmx.com>
2023-07-01 17:33:03 +0100

    First commit

---
+++ build/edukit_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/edukit_bot_ws/src/edukit_bot
+# Build directory: /home/pi/edukit_bot_ws/build/edukit_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
